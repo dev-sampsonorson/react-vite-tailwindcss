a1ed91a3af8fa1d795b196fa769c154a83ddaa9a
Sampson Orson Jackson <sampson.orson@gmail.com>
2024-03-15 11:26:02 +0100

    feat: use basic tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import './App.css'
 
 function App() {
   return (
-    <div className=''>
-      <h1 className=''>Hello World</h1>
+    <div className='p-8 h-screen bg-gray-300'>
+      <h1 className='text-3xl font-medium flex gap-3 items-center'>Hello World</h1>
     </div>
   )
 }
